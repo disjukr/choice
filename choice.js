02728112cecca33c67094f51a2b986ef53df7c60
JongChan Choi <disjukr@naver.com>
2014-05-04 01:57:55 +0900

    mock up

---
+++ choice.js
@@ -0,0 +1,20 @@
+function tokenize() {
+    return ['if', '(', 'true', ')', '{', '}'];
+}
+
+function parse(tokens) {
+    return [{type: 'if', condition: true, statements: []}];
+}
+
+function compile(ast) {
+    return 'if (true) {}'
+}
+
+function translate(sourceCode) {
+    var tokens = tokenize(sourceCode);
+    var ast = parse(tokens);
+    var code = compile(ast);
+    return code;
+}
+
+module.exports.translate = translate;
---
+++ cli.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+process.title = 'choice';
+
+var choice = require('./choice.js');
+var path = require('path');
+var fs = require('fs');
+
+var argv = process.argv.concat();
+argv.splice(0, 2);
+
+argv.forEach(function (arg) {
+    var p = path.normalize(arg);
+    var dir = path.dirname(p);
+    var base = path.basename(p, '.ch');
+    var source = fs.readFileSync(p, {encoding: 'utf8'});
+    var result = choice.translate(source);
+    var resultPath = path.join(dir, base + '.js');
+    fs.writeFileSync(resultPath, result, {encoding: 'utf8'});
+});
