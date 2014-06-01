509b24cfb5b018907453f8b69e1e724ff84357c9
JongChan Choi <disjukr@naver.com>
2014-06-02 05:21:53 +0900

    var, const

--- parser.js
+++ parser.js
@@ -1,4 +1,4 @@
 var Parser = require('jison').Parser;
 var parser = new Parser(require('./choice.json'));
-var result = parser.parse('for (1; 2; 3) statement;');
+var result = parser.parse('for ( var i = 0; 1; 2);');
 console.log(require('util').inspect(result, false, 10));
