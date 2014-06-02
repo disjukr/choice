017dafa1f7c6178536f7dee32ea9d8dc2cb18ad3
JongChan Choi <disjukr@naver.com>
2014-06-02 10:15:57 +0900

    comma

--- parser.js
+++ parser.js
@@ -1,4 +1,4 @@
 var Parser = require('jison').Parser;
 var parser = new Parser(require('./choice.json'));
-var result = parser.parse('for ( var i = 0; 1; 2);');
+var result = parser.parse('if (true, false, 1) true;');
 console.log(require('util').inspect(result, false, 10));
