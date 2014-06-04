05ba7ee5d0af759e74a8bf35abfa6dbec5ff3147
JongChan Choi <disjukr@naver.com>
2014-06-04 22:38:57 +0900

    access field

--- parser.js
+++ parser.js
@@ -1,4 +1,5 @@
 var Parser = require('jison').Parser;
 var parser = new Parser(require('./choice.json'));
-var result = parser.parse('val a = 1;');
+require('fs').writeFileSync(__dirname + '/temp.js', parser.generate());
+var result = parser.parse('val a = [1, 2, 3][field1].field2;');
 console.log(require('util').inspect(result, false, 10));
