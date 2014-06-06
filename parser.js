2884c93b2ea641ee51f0c28731baa7360bc4fc0e
JongChan Choi <disjukr@naver.com>
2014-06-06 16:07:29 +0900

    lambda

--- parser.js
+++ parser.js
@@ -1,5 +1,5 @@
 var Parser = require('jison').Parser;
 var parser = new Parser(require('./choice.json'));
 require('fs').writeFileSync(__dirname + '/temp.js', parser.generate());
-var result = parser.parse('val a = [1, 2, 3][field1].field2;');
+var result = parser.parse('val a = (a, b, c){1;};');
 console.log(require('util').inspect(result, false, 10));
