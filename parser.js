e5b2d559c660d5cbf24310290c8da13d14d32a20
JongChan Choi <disjukr@naver.com>
2014-06-06 18:06:20 +0900

    object literal

--- parser.js
+++ parser.js
@@ -1,5 +1,5 @@
 var Parser = require('jison').Parser;
 var parser = new Parser(require('./choice.json'));
 require('fs').writeFileSync(__dirname + '/temp.js', parser.generate());
-var result = parser.parse('val a = (a, b, c){1;};');
+var result = parser.parse('val a = {a: 1, b: 3};');
 console.log(require('util').inspect(result, false, 10));
