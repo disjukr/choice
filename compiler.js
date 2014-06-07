f4a5090cd55762107e455f544e808105c2099ad8
JongChan Choi <disjukr@naver.com>
2014-06-07 22:57:45 +0900

    access

--- compiler.js
+++ compiler.js
@@ -1,5 +1,8 @@
 exports.compile = function (ast) {
-    return transform(ast);
+    return [
+        'var __choice_access__, __choice_field__, __choice_member__;',
+        transform(ast)
+    ].join('\n');
 };
 
 function transform(node) {
@@ -14,9 +17,26 @@ function transform(node) {
     return node + '';
 }
 
+transform['access'] = function (node) {
+    return [
+        '(\n',
+        '__choice_access__ = ', transform(node.expression), ',\n',
+        '__choice_field__ = ', transform(node.field), ',\n',
+        '__choice_member__ = (',
+        '__choice_access__.__access__ ?\n',
+        '__choice_access__.__access__(__choice_field__) :\n',
+        '__choice_access__[__choice_field__]\n',
+        '),\n',
+        '(typeof __choice_member__ === "function") ?\n',
+        '__choice_member__.bind(__choice_access__) :\n',
+        '__choice_member__',
+        '\n)'
+    ].join('');
+};
+
 transform['call'] = function (node) {
     return [
-        transform(node.callee),
+        '(', transform(node.callee), ')',
         '(',
         node.arguments.map(transform).join(','),
         ')'
