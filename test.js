88373ac4c13780cb80bbec9abf3f7c13ab3e0b40
JongChan Choi <disjukr@naver.com>
2014-07-13 01:04:08 +0900

    parse match

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var parser = new Parser(require('./choice.json'));
 require('fs').writeFileSync(__dirname + '/temp.js', parser.generate());
 
 console.log('===== SOURCE =====');
-var source = 'for label (var key, value in {a: 1, b: 2, c: 3}) console.log(key, value);';
+var source = 'match(1){=2->3,4~5->6,7};';
 console.log(source);
 
 console.log('===== AST =====');
